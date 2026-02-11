d9432231b147373df9c5521e0bc871ddf9d7e34b
alaa <alaatoma58@gmail.com>
2026-02-11 02:30:00 +0200

    create project

---
+++ src/router.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+import Mainlayout from "./Layout/Mainlayout";
+import Home from "./Pages/Home/Home";
+import Cart from "./Pages/Cart/Cart";
+import Login from "./Pages/Auth/Login";
+import Register from "./Pages/Auth/Register";
+
+
+const router = createBrowserRouter([
+
+    {
+        path: '/',
+        element: <Mainlayout />, 
+        children: [
+            {
+                index: true, 
+                element: <Home />
+            },
+            {
+                path: '/Cart', 
+                element: <Cart />
+            },
+            {
+                path: '/Login', 
+                element: <Login />
+            },
+            {
+                path: '/Register', 
+                element: <Register />
+            }
+
+        ]
+    }
+]);
+
+export default router ;
